6060e95354e934592e20368bc25fee6c9f3c0d4b
AndrewAha <133350311+AndrewAha@users.noreply.github.com>
2025-06-11 10:51:40 +0800

    Add files via upload

--- ai-service/run.py
+++ ai-service/run.py
@@ -18,7 +18,30 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api.stt_api import *
 from api.tts_api import *
 from api.deepseek_api import *
+from api.media_api import *
+import threading
 
+think_end = False
+response = None
+
+def think_gif():
+    player = init_gifplayer("../think_gif/")
+    while not think_end:
+        show_gif(player)
+
+
+def ai_text_response(conversation, input_text):
+    logging.debug("ai_text_response start!")
+    ms_start = int(time.time() * 1000)
+
+    result = conversation.invoke(input=input_text)
+    logging.debug(f"ai_text_response response: {result}")
+    result = result['response']
+    logging.debug(f"text response: {result}")
+    ms_end = int(time.time() * 1000)
+    logging.debug(f"ai_text_response end, delay = {ms_end - ms_start}ms")
+    response =  result
+    think_end = True
 
 def main():
     load_dotenv("../.env")
@@ -35,10 +58,20 @@ def main():
         if "结束" in user_input:
             break
         
-        response = ai_text_response(conversation=conversation, input_text=user_input)
+        deepseek_task = threading.Thread(target=ai_text_response, args=(conversation, user_input))
+        deepseek_task.start()
+
+        think_end = False
+        th_gif_thread = threading.Thread(target=think_gif)
+        th_gif_thread.start()
+
+        deepseek_task.join()
+        th_gif_thread.join()
+
         print(response)
         
         tts(response)
+
         
     print("AI end!")
 
